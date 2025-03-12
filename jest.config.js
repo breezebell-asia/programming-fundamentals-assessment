33a6d75eea28116c19e9166f6d91065e94079680
apexbridge-tech <apexbridge.tech@gmail.com>
2025-03-12 11:24:00 +0500

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testMatch: ['<rootDir>/test-*/test.js'],
+  collectCoverage: true,
+  coverageReporters: ['text', 'html'],
+  verbose: true,
+  testTimeout: 30000,
+  bail: false
+};
